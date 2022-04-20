d236978a29e8672cf4522e661e2c39cb85449a24
MarsonM <mars-message@outlook.com>
2022-04-20 05:36:09 +0500

    ДЗ за четвертый урок.
    Сделал ДЗ за четвертый урок. Спасибо за проверку.

---
+++ scriptTask_1_2.js
@@ -0,0 +1,25 @@
+
+//ДЗ первое и второе задания
+let str = `One: 'Hi Mary.' Two: 'Oh, hi.'
+One: 'How are you doing?'
+Two: 'I'm doing alright. How about you?'
+One: 'Not too bad. The weather is great isn't it?'
+Two: 'Yes. It's absolutely beautiful today.'
+One: 'I wish it was like this more frequently.'
+Two: 'Me too.'
+One: 'So where are you going now?'
+Two: 'I'm going to meet a friend of mine at the department store'
+One: 'Going to do a little shopping?'
+Two: 'Yeah, I have to buy some presents for my parents.'
+One: 'What's the occasion?'
+Two: 'It's their anniversary.'
+One: 'That's great. Well, you better get going. You don't want to be late.'
+Two: 'I'll see you next time.'
+One: 'Sure.' Bye.'
+`
+
+//Заменяет все ' на "
+console.log(str.replace(/'/g, `"`));
+
+// Заменяет только ' вне конструкции типа aren't
+console.log(str.replace(/\B[']|[']\B/g, `"`));
